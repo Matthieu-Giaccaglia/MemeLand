02369e18b18d2fa05ee5892fd40eda8d64e9e42a
Atchy-Dalama Julien <julien.atchy-dalama@etu.umontpellier.fr>
2020-11-26 16:06:03 +0100

    Replace updated.php

--- PHP/view/produit/updated.php
+++ PHP/view/produit/updated.php
@@ -1,5 +1,5 @@
 <?php
 $vIdProduit = htmlspecialchars($idProduit);
-echo "<p>La voiture d'immatriculation $vIdProduit a bien été mise à jour.</p>";
+echo "<p>Le produit $vIdProduit a bien été mise à jour.</p>";
 require File::build_path(array('view',self::$object,'list.php'));
 ?>
