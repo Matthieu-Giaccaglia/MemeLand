8f5310149c18c880c067b3275081bafc795d0e0a
Matthieu Giaccaglia <matthieu.giaccaglia@etu.umontpellier.fr>
2020-12-06 16:28:50 +0100

    connected()

--- PHP/lib/Session.php
+++ PHP/lib/Session.php
@@ -6,9 +6,12 @@ class Session {
 
     public static function is_admin() {
 
-        
-
         return (!empty($_SESSION['admin']) && $_SESSION['admin']);
+    }
+
+    public static function is_connected(){
+
+        return (!empty($_SESSION['login']));
     }
 }
 ?>
