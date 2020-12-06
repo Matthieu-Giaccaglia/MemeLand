6e2be5133b4724fcbc8f80d177e62c5a8d0b189f
Walter Deneuville <walterdeneuville@gmail.com>
2020-12-06 18:33:06 +0100

    panier

--- PHP/view/utilisateur/panier.php
+++ PHP/view/utilisateur/panier.php
@@ -1,36 +1,44 @@
 <section>
     <h3>Panier :</h3>
     <main>
-        <ul>
 
 <?php
-foreach ($tab_panier as $prod_p) {
-    $p = ModelProduit::select($prod_p);
-    
-    $vidProduitURL = rawurlencode($p->get("id_produit"));
-    $image = $p->get("image");
-    $nom = $p->get("nom");
-    $categorie = $p->get("categorie_id");
-    $prix = $p->get("prix");
-    
-    echo <<< EOT
-        <li> 
-            <a href="?controller=produit&action=read&id_produit=$vidProduitURL"><img src="./public/images/produit/$image" alt="produit_image">
-                <table>
-                    <tr>
-                    <td>$nom</td>
-                    <td>$categorie</td>
-                    <td>$prix <strong>€</strong></td>
-                    </tr>
-                </table>
-            </a>
-        </li>
+$prixTot = 0;
+if(sizeof($tab_panier)==0){
+    echo "Votre panier est vide";
+}else{
+    echo "<ul>";
+    foreach ($tab_panier as $prod_p) {
+        $p = ModelProduit::select($prod_p);
+        
+        $vidProduitURL = rawurlencode($p->get("id_produit"));
+        $image = $p->get("image");
+        $nom = $p->get("nom");
+        $categorie = $p->get("categorie_id");
+        $prix = $p->get("prix");
+        
+        $prixTot = $prixTot + $p->get("prix");
+
+        echo <<< EOT
+            <li> 
+                <a href="?controller=produit&action=read&id_produit=$vidProduitURL"><img src="./public/images/produit/$image" alt="produit_image">
+                    <table>
+                        <tr>
+                        <td>$nom</td>
+                        <td>$categorie</td>
+                        <td>$prix <strong>€</strong></td>
+                        </tr>
+                    </table>
+                </a>
+            </li>
 EOT;
+    }
+    echo "</ul>";
 }
 ?>
-        </ul>
     </main>
     <div class="button">
+        <?php if($prixTot != 0) echo "<p> Prix total : $prixTot</p>";?>
         <input class="b_input" type="submit" value="Payer"/>
     </div>
 </section>
